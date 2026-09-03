cf68ef8dec35ddbe8df265e852fecb5c547613d9
lavanya-jain13 <er.jainlavanya@gmail.com>
2026-09-04 03:45:33 +0530

    chore: configure production database migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -17,4 +17,24 @@ module.exports = {
       directory: "./seeds",
     },
   },
+
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
 };
